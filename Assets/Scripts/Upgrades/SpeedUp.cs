50db5e81ca544510be3c20b72eed00c4223003ec
dawiddzhafarov <dawid.dzhafarov@gmail.com>
2022-05-21 14:40:47 +0200

    upgrademenu update

--- Assets/Scripts/Upgrades/SpeedUp.cs
+++ Assets/Scripts/Upgrades/SpeedUp.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Upgrades/SpeedUp")]
-public class NewBehaviourScript : PowerUps {
+public class SpeedUp : PowerUps {
     public float extraSpeed;
         
     public override void Apply() {
