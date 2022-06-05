e66432de64b95f35e7c7df255ecb248e388b9e74
dawiddzhafarov <dawid.dzhafarov@gmail.com>
2022-06-05 17:14:56 +0200

    instructions fix

--- Assets/Scripts/Instructions.cs
+++ Assets/Scripts/Instructions.cs
@@ -22,7 +22,6 @@ public class Instructions : MonoBehaviour
     {
         MenuCanvas.SetActive(true);
         InstructionsCanvas.SetActive(false);
-        //Time.timeScale = 1f;
         isSwitchedOn = false;
     }
 
@@ -30,7 +29,6 @@ public class Instructions : MonoBehaviour
     {
         MenuCanvas.SetActive(false);
         InstructionsCanvas.SetActive(true);
-        //Time.timeScale = 0f;
         isSwitchedOn = true;
     }
 }
